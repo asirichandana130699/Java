20f17fb405fbc459bda513291845596dfda2958f
asirichandana130699 <asirichandana130699@gmail.com>
2024-01-10 23:29:22 -0600

    student class

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    String name = "Siri Chandana A";
+    public String getName() {
+        return name;
+    }
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.getName());
+    }
+}
