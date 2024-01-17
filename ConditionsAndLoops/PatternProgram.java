73759ad568d491fe6dea059b3006a85e96bea18c
asirichandana130699 <asirichandana130699@gmail.com>
2024-01-17 09:48:41 -0600

    Conditions and Loops

---
+++ ConditionsAndLoops/PatternProgram.java
@@ -0,0 +1,15 @@
+public class PatternProgram {
+    public static void main(String[] args) {
+        int rows = 5;
+        for(int i = 0; i < rows; i++) {
+            int cols = rows - i - 2;
+            for(int j = cols; j >= 0; j--) {
+                System.out.print("+ ");
+            }
+            for(int j = cols; j < rows; j++) {
+                System.out.print("- ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
