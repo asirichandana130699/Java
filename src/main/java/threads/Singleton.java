9038af8b2afc3233d50975cf9b8624d4e1625e9b
asirichandana130699 <asirichandana130699@gmail.com>
2024-02-11 20:54:25 -0600

    final updates

--- src/main/java/threads/Singleton.java
+++ src/main/java/threads/Singleton.java
@@ -1,4 +1,4 @@
-package thread;
+package threads;
 
 public class Singleton {
     private static Singleton instance;
--- src/test/java/threads/OddEvenPrinterTest.java
+++ src/test/java/threads/OddEvenPrinterTest.java
@@ -1,4 +1,4 @@
-package thread;
+package threads;
 
 import org.junit.Assert;
 import org.junit.Test;
