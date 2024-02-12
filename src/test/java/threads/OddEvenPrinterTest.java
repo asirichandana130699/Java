20d2e68d9fc3484bc31e0ddc783f3cb275182f53
asirichandana130699 <asirichandana130699@gmail.com>
2024-02-11 20:17:45 -0600

    exceptions and threads

---
+++ src/test/java/threads/OddEvenPrinterTest.java
@@ -0,0 +1,13 @@
+package threads;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OddEvenPrinterTest {
+    OddEvenPrinter evenOrOdd = new OddEvenPrinter();
+    @Test
+    public void testEvenOrOdd() {
+        Assert.assertEquals(4, (int) evenOrOdd.getEvenOdd(10)
+                .get(3));
+    }
+}
